431ed95a6557b48fbc40d26a4360f19f02db6acb
choky59 <gypsygears@gmail.com>
2022-12-14 20:23:21 -0700

    feat: added semantic versioning

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,5 @@
-
-export const test = {};
+export { Button } from './components/example/button/Button';
+export { Card } from './components/ui/card/Card';
+export { Header } from './components/example/header/Header';
+export { Label } from './components/ui/label/Label';
+export { Page } from './components/example/page/Page';
